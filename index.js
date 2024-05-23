e713cfbea9b31c1dd195fb0d4ea36f51a944d2e8
sjsusu <serena.shirlin@gmail.com>
2024-05-22 20:44:53 -0400

    Add Grid Size and Clear Buttons

--- index.js
+++ index.js
@@ -1,21 +1,59 @@
 const grid = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-    let line = document.createElement("div");
-    line.classList.add("line");
+// Make Default 16x16 grid
+makeGrid(16);
 
-    for(let j = 0; j<16; j++) {
-        let box = document.createElement("div");
-        box.classList.add("box");
+grid.addEventListener("mouseover", (e) => {
+    let target = e.target;
+    if (target.className === "box") {
+        target.setAttribute("style", "background-color: rgba(0,0,0,0.1);");
+    }
+});
 
-        line.appendChild(box);
+const gridSize = document.querySelector(".grid-size");
+
+gridSize.addEventListener("click", () => {
+    let text = "Please enter number of boxes per row for grid."
+    let answer = prompt(text);
+
+    while (isNaN(answer) || Number(answer) > 100 || Number(answer) < 1) {
+        if (answer === null){
+            return;
+        }
+
+        alert("Invalid answer or too large, try again.");
+        answer = prompt(text);
     }
 
-    grid.appendChild(line);
+    const size = Number(answer);
+    makeGrid(size);
 
-}
-
-grid.addEventListener("mouseover", (e)=>{
-    let target = e.target;
-    target.setAttribute("style", "background-color: grey;");
 });
+
+const clear = document.querySelector(".clear");
+
+clear.addEventListener("click", () => {
+    const boxes = document.querySelectorAll(".box")
+
+    for (let box of boxes) {
+        box.setAttribute("style", "background-color: white;");
+    }
+});
+
+function makeGrid(size) {
+    grid.replaceChildren();
+    for (let i = 0; i < size; i++) {
+        let line = document.createElement("div");
+        line.classList.add("line");
+
+        for (let j = 0; j < size; j++) {
+            let box = document.createElement("div");
+            box.classList.add("box");
+
+            line.appendChild(box);
+        }
+
+        grid.appendChild(line);
+
+    }
+}
